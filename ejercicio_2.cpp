51b7765b8aec3122fe59afd1726f048a55210dcc
AdrianUcjc <160497633+AdrianUcjc@users.noreply.github.com>
2024-10-18 12:17:00 +0200

    ejercicio_2.cpp

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -1,34 +1,46 @@
-// Haz que el LED PIN 13 parpadee regularmente, PIN 12 dos veces mas lento, PIN 11 cuatro veces más lento.
+// Se pide crear un semáforo de coches con Arduino. 
+// 1. Se enciende el led de color verde durante 5 segundos. 
+// 2. Se parpadea el led de color verde 3 veces durante 2 segundos. 
+// 3. Se enciende después el led de color amarillo durante 3 segundos. 
+// 4. Se enciende al final el led de color rojo durante 5 segundos. 
 
 void setup() {
-  int pin = 2;
-  // put your setup code here, to run once:
-  pinMode(pin,OUTPUT); // Establece "pin" como salida
-  int nip = 4;
-  // put your setup code here, to run once:
-  pinMode(nip,OUTPUT); // Establece "pin" como salida
-  int nep = 7;
-  // put your setup code here, to run once:
-  pinMode(nep,OUTPUT); // Establece "pin" como salida
-  
+  int verde = 2; // Código de "setup", para el LED verde.
+  pinMode(verde,OUTPUT); // Establece "pin" como salida.
+  int amarillo = 4; // Código de "setup", para el LED verde.
+  pinMode(amarillo,OUTPUT); // Establece "pin" como salida.
+  int rojo = 7; // Código de "setup", para el LED rojo.
+  pinMode(rojo,OUTPUT); // Establece "pin" como salida
+
 }
+
 void loop() {
-  int pin = 2;
-  // put your main code here, to run repeatedly:
-  digitalWrite(pin,HIGH); // Activa "pin"
-  delay(100); // Pausa un segundo
-  digitalWrite(pin,LOW); // Desactiva "pin" delay(1000);
-  delay(10); // Pausa un segundo
-  int nip = 4;
-  // put your main code here, to run repeatedly:
-  digitalWrite(nip,HIGH); // Activa "pin"
-  delay(100); // Pausa un segundo
-  digitalWrite(nip,LOW); // Desactiva "pin" delay(1000);
-  delay(20); // Pausa un segundo
-  int nep = 7;
-  // put your main code here, to run repeatedly:
-  digitalWrite(nep,HIGH); // Activa "pin"
-  delay(100); // Pausa un segundo
-  digitalWrite(nep,LOW); // Desactiva "pin" delay(1000);
-  delay(40); // Pausa un segundo
+
+  int verde = 2; // Inicializamos nuestro pin asignado al LED verde.
+  digitalWrite(verde,HIGH); // Activa el pin verde.
+  delay(5000); // Pausa de 5 segundos.
+  digitalWrite(verde,LOW); // Desactiva "pin".
+  delay(1000); // Pausa  de 1 segundo
+  digitalWrite(verde,HIGH); // Activa "pin".
+  delay(2000); // Pausa de 2 segundos.
+  digitalWrite(verde,LOW); // Desactiva "pin".
+  delay(1000); // Pausa de 2 segundos.
+  digitalWrite(verde,HIGH); // Activa "pin".
+  delay(2000); // Pausa de 2 segundos.
+  digitalWrite(verde,LOW); // Desactiva "pin".
+  delay(1000); // Pausa un segundo.
+  digitalWrite(verde,HIGH); // Activa "pin".
+  delay(2000); // Pausa de 2 segundos.
+  digitalWrite(verde,LOW); // Desactiva el LED verde.
+  
+  int amarillo = 4; // Inicializamos nuestro pin asignado al LED amarillo.
+  digitalWrite(amarillo,HIGH); // Activa el pin amarillo.
+  delay(3000); // Pausa de 3 segundos.
+  digitalWrite(amarillo,LOW); // Desactiva el LED amarillo.
+
+  int rojo = 7; // Inicializamos nuestro pin asignado al LED rojo.
+  digitalWrite(rojo,HIGH); // Activa el LED rojo.
+  delay(5000); // Pausa de 5 segundos.
+  digitalWrite(rojo,LOW); // Desactiva el LED.
+
 }
